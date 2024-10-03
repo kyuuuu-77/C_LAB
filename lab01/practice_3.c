6b39d8b13df33809eae4c0ce666b9cec49681bca
kyuuuu-77 <tjrbqls1123@naver.com>
2024-10-03 16:17:54 +0900

    10/03 lab01 committed

---
+++ lab01/practice_3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello\n");
+    printf("World\n");
+    printf("KNU!\n");
+    return 0;
+}
---
+++ lab01/practice_4.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("He said \"Hello\"\n");
+    return 0;
+}
