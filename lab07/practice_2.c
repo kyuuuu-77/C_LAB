3584458200fe44cf376e767c6401dd117940568d
kyuuuu-77 <tjrbqls1123@naver.com>
2024-10-18 19:30:51 +0900

    10/18 lab07 Updated

--- lab07/practice_2.c
+++ lab07/practice_2.c
@@ -1,6 +1,23 @@
 #include "stdio.h"
 
 int main() {
+    int x, y;
+
+    scanf("%d %d", &x, &y);
+
+    if (x == 0 || y == 0) {
+        printf("Error");
+    } else {
+        if (x > 0 && y > 0) {
+            printf("x=%d y=%d, 제%d사분면\n", x, y, 1);
+        } else if (x < 0 && y > 0) {
+            printf("x=%d y=%d, 제%d사분면\n", x, y, 2);
+        } else if (x < 0 && y < 0) {
+            printf("x=%d y=%d, 제%d사분면\n", x, y, 3);
+        } else {
+            printf("x=%d y=%d, 제%d사분면\n", x, y, 4);
+        }
+    }
 
     return 0;
 }
--- lab07/practice_4.c
+++ lab07/practice_4.c
@@ -1,6 +1,39 @@
 #include "stdio.h"
 
 int main() {
+    int mode, min, fee;
+    double distance;
+
+    printf("속도 옵션과 이동시간을 입력하세요 => ");
+    scanf("%d %d", &mode, &min);
+
+    switch (mode) {
+        case 2:
+            fee = 180 * min;
+            distance = (25000. / 60) * min;
+            break;
+        case 1:
+            fee = 170 * min;
+            distance = (20000. / 60) * min;
+            break;
+        case 0:
+            fee = 100 * min;
+            distance = (15000. / 60) * min;
+            break;
+        default:
+            fee = -1;
+    }
+
+    if (fee == -1) {
+        printf("속도 옵션을 잘못 입력하였습니다.\n");
+    } else {
+        printf("킥보드 이용 요금 = %d원\t", fee);
+        if (distance >= 1000) {
+            printf("이동거리 = %.2lfkm\n", distance/1000.);
+        } else {
+            printf("이동거리 = %.2lfm\n", distance);
+        }
+    }
 
     return 0;
 }
