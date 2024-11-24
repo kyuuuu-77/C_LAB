89d18dcba243bf469f47f3e4f1ab4f7090b22a27
Kyuuuu <kyuuuu77@yahoo.co.jp>
2024-11-24 12:32:33 +0900

    11/24 12:32 lab12 Updated

--- lab12/stock.c
+++ lab12/stock.c
@@ -2,10 +2,10 @@
 
 extern int beer, milk, snack, ice;
 void stock(int menu, int num) {
-    if (num < 0) {
+    if (num < 0) {      // 재고는 0개 이상이어야 하므로
         printf("재고는 0개 이상이어야 합니다.\n");
     } else {
-        switch (menu) {
+        switch (menu) {     // 물품 코드에 따라서 물품의 재고를 변경
             case 1:
                 beer = num;
                 break;
