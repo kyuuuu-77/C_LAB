dfbb223b81357ea0fad4971f0cb865ddf8d047d9
Kyuuuu <kyuuuu77@yahoo.co.jp>
2024-12-04 11:52:37 +0900

    12/04 11:52 lab14 Updated

--- lab14/practice_2.c
+++ lab14/practice_2.c
@@ -12,22 +12,21 @@ int main() {
             printf("단어장을 종료합니다.\n");
             break;
         }
-        if (row >= 10 || row < 0) {
-            printf("다시 입력하세요!\n");
-            continue;
-        }
 
-        printf("%d번째 칸에 저장할 단어를 입력하세요 : ", row);
-        scanf_s("%s", dict[row], sizeof(dict[row]));
-        printf("%d번째 칸에 %s를 저장했습니다!\n", row, dict[row]);
+        if (row >= 0 && row < 10) {
+            printf("%d번째 칸에 저장할 단어를 입력하세요 : ", row);
+            scanf_s("%s", dict[row], sizeof(dict[row]));
+            printf("%d번째 칸에 %s를 저장했습니다!\n", row, dict[row]);
 
-        for (int i = 0; i < 10; i++) {
-            if (dict[i][0] != 0) {
-                printf("%s ", dict[i]);
+            for (int i = 0; i < 10; i++) {
+                if (dict[i][0] != 0) {
+                    printf("%s ", dict[i]);
+                }
             }
+            printf("\n");
         }
-        printf("\n");
+        else {
+            printf("다시 입력하세요!\n");
+        }
     }
-
-    return 0;
 }
