efac461145e214634dff356ab50ab2ee5d2b7072
Kyuuuu <kyuuuu77@yahoo.co.jp>
2024-12-09 19:45:45 +0900

    12/09 19:45 lab14 Updated, lab15 Deleted

--- lab14/practice_2.c
+++ lab14/practice_2.c
@@ -1,31 +1,34 @@
 #include <stdio.h>
 
 int main() {
-    char dict[10][12] = {0};
-    int row;
+    char dict[10][13] = {0};    // 배열을 선언하고 모든 칸에다가 0의 값을 대입
+    int column;    // 입력 받은 행(column)을 저장할 변수 선언
+    // 12/19 19:13 수정 : 변수 이름 row->column으로 변경
 
     while (1) {
         printf("저장할 칸을 입력하세요 : ");
-        scanf_s("%d", &row);
+        scanf_s("%d", &column);
 
-        if (row == -1) {
+        if (column == -1) {    // -1을 입력하면 단어장 종료
             printf("단어장을 종료합니다.\n");
             break;
         }
 
-        if (row >= 0 && row < 10) {
-            printf("%d번째 칸에 저장할 단어를 입력하세요 : ", row);
-            scanf_s("%s", dict[row], sizeof(dict[row]));
-            printf("%d번째 칸에 %s를 저장했습니다!\n", row, dict[row]);
+        if (column >= 0 && column < 10) {   // 저장할 수 있는 칸일 경우에 (단어장의 크기가 10이기 때문에 0~9칸에 저장할 수 있다)
+            printf("%d번째 칸에 저장할 단어를 입력하세요 : ", column);
+            scanf_s("%s", dict[column], 13);
+            // scanf("%s", dict[column]);
+            // 문자열과 배열일 경우에는 scanf와 scanf_s의 사용 방법이 다릅니다. 주의!
+            printf("%d번째 칸에 %s를 저장했습니다!\n", column, dict[column]);  // 저장한 칸(행)과 저장한 문자열을 출력
 
-            for (int i = 0; i < 10; i++) {
-                if (dict[i][0] != 0) {
-                    printf("%s ", dict[i]);
+            for (int i = 0; i < 10; i++) {  // 반복문을 통해서 단어장의 단어를 모두 출력
+                if (dict[i][0] != 0) {  // 단어장에 저장된 값이 0이 아니면 = 즉 단어를 저장한 상태이면
+                    printf("%s ", dict[i]);     // 해당 칸에 저장된 단어를 출력
                 }
             }
             printf("\n");
         }
-        else {
+        else {      // 올바르지 않은 칸에 저장하려고 시도하는 경우
             printf("다시 입력하세요!\n");
         }
     }
--- lab14/practice_3.c
+++ lab14/practice_3.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 
 int main() {
-    char english[20];
+    char english[20];   // 하나의 문자열을 저장할 배열 선언
 
     printf("문자열을 입력하세요 : ");
-    scanf_s("%s", english, 20);
+    scanf_s("%s", english, 20);     // 문자열의 중간에 띄어쓰기가 있지 않는 이상 왠만하면 scanf나 scanf_s를 사용하는 것을 권장합니다.
+    // 띄어쓰기가 있는 경우에는 gets를 사용
     // gets(english);
-    // fgets(english, 20, stdin);
 
-    for (int i = 0; i < english[i] != '\0'; i++) {
-        if (english[i] >= 'a' && english[i] <= 'z') {
-            english[i] -= 32;
-        } else if (english[i] >= 'A' && english[i] <= 'Z') {
-            english[i] += 32;
-        } else if (english[i] >= '0' && english[i] <= '9') {
-            english[i] = ' ';
-        } else {
-            english[i] = '_';
+    // 아스키코드 표를 참고할 것
+    for (int i = 0; i < english[i] != '\0'; i++) {      // 문자열을 저장하고 나머지 공간에는 \0이 저장되어 있기 때문에 반복문은 \0이 저장된 바로 전의 칸까지 반복합니다.
+        if (english[i] >= 'a' && english[i] <= 'z') {   // 소문자일 경우
+            english[i] -= 32;   // 대문자로 변경
+        } else if (english[i] >= 'A' && english[i] <= 'Z') {    // 대문자일 경우
+            english[i] += 32;   // 소문자로 변경
+        } else if (english[i] >= '0' && english[i] <= '9') {    // 숫자일 경우
+            english[i] = ' ';   // 빈칸으로 변경
+        } else {    // 그 외에 특수문자일 경우
+            english[i] = '_';   // 언더바로 변경
         }
     }
     printf("출력결과 : %s", english);
